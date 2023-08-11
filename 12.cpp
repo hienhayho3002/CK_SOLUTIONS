9cb6a4d0320c8decb6597c3ff6e4a97681ac2c20
hienhayho3002 <119850206+hienhayho3002@users.noreply.github.com>
2023-08-11 16:01:23 +0700

    Create 12.cpp

---
+++ 12.cpp
@@ -0,0 +1,61 @@
+#pragma GCC optimize("Ofast","inline","-ffast-math")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define arr array
+#define TIME (1.0*clock()/CLOCKS_PER_SEC)
+typedef long long ll;
+typedef uint64_t ull;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+#define all(v) (v).begin(), (v).end()
+#define it(v, x) std::iota(all(v), (x))
+#define acc(v, x) std::accumulate(all(v), (x))
+#define eb emplace_back
+#define endl '\n'
+
+void solve(){
+    int n, x;
+    cin >> n >> x;
+    int a[n];
+    for (int i = 0; i < n; ++i) cin >> a[i];
+    sort(a, a + n);
+	  ll s = 0;
+	  int l = 0, r = n - 1;
+  	ll MAX = 0;
+  	while(l < r){
+  		s = a[l] + a[r];
+  		if (s == x){
+  			MAX = x;
+  			break;
+  		}
+  		else if (s < x){
+  			MAX = max(MAX, s);
+  			l++;
+  		}
+  		else r--;
+  	}
+  	cout << MAX;
+}
+
+
+int main()
+{
+	//srand(time(NULL));
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+	
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    
+    //int t; cin >> t; for (int i = 1; i <= t; ++i)
+    solve();
+    
+    #ifndef ONLINE_JUDGE
+    cerr << "Time elapsed: " << TIME << "s.\n";
+    #endif
+    return 0;
+}
