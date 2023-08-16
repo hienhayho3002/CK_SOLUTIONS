688c723d5f61634ea2b54e3bf00573039f3c13a6
hienhayho3002 <119850206+hienhayho3002@users.noreply.github.com>
2023-08-16 23:49:56 +0700

    Create 13_10points_sol_2.cpp

---
+++ 13_10points_sol_2.cpp
@@ -0,0 +1,56 @@
+#pragma GCC optimize("Ofast","inline","-ffast-math")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define arr array
+#define TIME (1.0*clock()/CLOCKS_PER_SEC)
+typedef long long ll;
+typedef uint64_t ull;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+#define all(v) (v).begin(), (v).end()
+#define it(v, x) std::iota(all(v), (x))
+#define acc(v, x) std::accumulate(all(v), (x))
+#define eb emplace_back
+#define endl '\n'
+
+ll a[10005];
+ll f[10005];
+
+// Đệ quy có nhớ
+ll Try(int i){
+    if (i == 0) return 0;
+    if (i == 1) return a[1];
+    if (f[i]) return f[i];
+    return f[i] = max(Try(i - 1), Try(i - 2) + a[i]);
+}
+
+void solve(){
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; ++i)
+        cin >> a[i];
+    cout << Try(n);
+}
+
+
+int main()
+{
+    //srand(time(NULL));
+    #ifndef ONLINE_JUDGE
+    freopen("TASK.inp", "r", stdin);
+    freopen("TASK.out", "w", stdout);
+    #endif
+    
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    
+    //int t; cin >> t; for (int i = 1; i <= t; ++i)
+    solve();
+    
+    #ifndef ONLINE_JUDGE
+    cerr << "Time executed: " << TIME << "s.\n";
+    #endif
+    return 0;
+}
